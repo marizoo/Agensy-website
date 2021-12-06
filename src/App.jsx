4555049b69a8e2ea9d4138f6b0ea1ebcce14dad1
marizoo <marizoo@meow.woof>
2021-12-06 22:05:22 +0700

    initial start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+`
+
+const App = () => {
+  return (
+    <>
+  <Container>
+    Hello world!
+    </Container>
+  </>
+  )
+};
+
+export default App;
