32199cbf603c9c5f73ed50f835ad32cb8a99fc7d
marizoo <marizoo@meow.woof>
2021-12-06 23:39:47 +0700

    completed navbar, animatedshapes, and IntroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+import Navbar from './components/Navbar';
+import Intro from './pages/Intro';
+
+
+const App = () => {
+  return (
+    <>
+    <Container>
+        <Navbar />
+        <Intro />
+        <IntroShape />
+    </Container>
+  </>
+  )
+};
+
+export default App;
+
 
 const Container = styled.div`
   height: 100vh;
@@ -6,14 +24,36 @@ const Container = styled.div`
   position: relative;
 `
 
-const App = () => {
-  return (
-    <>
-  <Container>
-    Hello world!
-    </Container>
-  </>
-  )
-};
+const Shape = css`
+width: 100%;
+height: 100%;
+position: absolute;
+top: 0;
+left: 0;
+z-index: -1;
+`
 
-export default App;
+const IntroShape = styled.div`
+${Shape}
+clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+background-color: crimson;
+`
+
+const FeatureShape = styled.div`
+${Shape}
+clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
+background-color: pink;
+
+`
+
+const ServiceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 33% 0%, 33% 100%, 0 100%);
+  background-color: #f88497;
+`
+
+const PriceShape = styled.div`
+${Shape}
+  clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
+  background-color: crimson;
+`
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+
+const Navbar = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Logo>Agency</Logo>
+                    <Menu>
+                        <MenuItem>Home</MenuItem>
+                        <MenuItem>Features</MenuItem>
+                        <MenuItem>Services</MenuItem>
+                        <MenuItem>Pricing</MenuItem>
+                        <MenuItem>Contact</MenuItem>
+                    </Menu>
+                </Left>
+                <Button>JOIN TODAY</Button>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Navbar
+
+const Container = styled.div`
+height: 50px;
+`
+
+const Wrapper = styled.div`
+padding: 10px 20px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`
+
+const Left = styled.div`
+width: 60%;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`
+
+const Logo = styled.span`
+font-weight: bold;
+font-size: 40px;
+text-decoration: underline crimson;
+`
+
+const Menu = styled.ul`
+display: flex;
+list-style: none;
+
+
+@media only screen and (max-width: 480px) {
+    display: none;
+}
+`
+
+const MenuItem = styled.li`
+margin-right: 30px;
+font-size: 20px;
+font-weight: 500;
+color: gray;
+cursor: pointer;
+`
+
+const Button = styled.button`
+border: 2px solid white;
+padding: 10px 15px;
+background-color: crimson;
+color: white;
+font-weight: bold;
+border-radius: 10px;
+cursor: pointer;
+`
